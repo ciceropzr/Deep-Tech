3f6aa3ddd0fb566899aa6e7a9b496f650a4af7ec
Cicero Matos <ciceropzr@gmail.com>
2018-04-09 14:55:20 -0300

    CSS Ajustes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -71,14 +71,10 @@
     casespan.classList.remove('cases-show')
   };
 
-  // Get the modal
-  var modal = document.getElementById('myModal');
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var modal = document.getElementById('myModal');  // Get the modal
+  var btn = document.getElementById("myBtn");   // Get the button that opens the modal
+  var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 
   
   // When the user clicks the button, open the modal 
@@ -88,13 +84,13 @@
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
-      modal.style.display = "none";
+      modal.classList.toggle('modal-show')
   }
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
       if (event.target == modal) {
-          modal.style.display = "none";
+          modal.classList.toggle('modal-show');
       }
   };
 })();
